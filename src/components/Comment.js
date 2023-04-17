530c4985e4253d5b7f7e970def1c571dd5683b22
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-17 09:43:32 -0400

    add likes/poops buttons & states to each comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Comment({comment}) {
+    const [likes, setLikes] = useState(0)
+    const [poops, setPoops] = useState(0)
+
+    function addLikes() {
+        setLikes(likes => likes + 1)
+    }
+
+    function addPoops() {
+        setPoops(poops => poops + 1)
+    }
+
     return (
         <div>
             <h4>{comment.user}</h4>
             <p>{comment.comment}</p>
+            <button onClick={addLikes}>{likes} ❤️ </button>
+            <button onClick={addPoops}>{poops} 💩 </button>
         </div>
     )
 }
