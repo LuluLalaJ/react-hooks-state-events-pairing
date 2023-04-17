8745871ede6615ae311e58dfe318902483f9c7e9
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-17 10:18:36 -0400

    create search bar but state is kinda iffy

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Comment from "./Comment";
 
 function CommentsContainer({comments}) {
-    console.log(comments)
-    const renderComments = comments.map(comment => <Comment key={comment.id} comment={comment}/>)
+    const [displayedComments, setDisplayedComments] = useState(comments)
+    // const [username, setUsername] = useState('')
+    const renderComments = displayedComments.map(comment => <Comment key={comment.id} comment={comment}/>)
+
+    function handleSearch(username) {
+        const filteredComments = comments.filter(comment => comment.user.toLowerCase() === username.toLowerCase())
+        setDisplayedComments(filteredComments)
+    } // feel iffy about this search
+
+    
+    
     return (
         <div>
+            <label> Search comments by username
+                <input onChange={(e)=>handleSearch(e.target.value)} placeholder="type username"/>
+            </label>
             {renderComments}
         </div>
     )
